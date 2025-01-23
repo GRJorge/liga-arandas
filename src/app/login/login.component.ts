bdf5bf5c69ad4c9c1860f9276c68eaa935b9ed5a
GRJorge <deb.devel@outlook.es>
2025-01-23 01:10:49 -0600

    Validate login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+}
