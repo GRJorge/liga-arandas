fd8c93ffda8d4d25f52c08f6fe8d9c1deefc798c
GRJorge <deb.devel@outlook.es>
2025-01-23 01:01:22 -0600

    Theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ export const theme = {
       2: '#3d3d3d',
     },
     light: '#f3f3f3',
-    green: '#126716',
+    green: '#0ABF7D',
+    blue: '#0468BF',
+    yellow: '#F2B33D',
   },
   fontFamily: {
     serif: 'Lexend, serif',
