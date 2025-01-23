b14a7575ed6644c43ed91c95d6bdb6bd5ba5c47c
GRJorge <deb.devel@outlook.es>
2025-01-23 00:45:20 -0600

    Global styles and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ['./src/**/*.{html,ts}'];
 export const theme = {
+  colors: {
+    transparent: 'transparent',
+    dark: {
+      1: '#222222',
+      2: '#3d3d3d',
+    },
+    light: '#f3f3f3',
+    green: '#126716',
+  },
+  fontFamily: {
+    serif: 'Lexend, serif',
+    title: 'Tourney, serif',
+  },
   extend: {},
 };
 export const plugins = [];
