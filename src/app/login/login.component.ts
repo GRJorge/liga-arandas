43470f903b627ca107e11938ea1db65059b51ec1
GRJorge <deb.devel@outlook.es>
2025-02-06 02:50:37 -0600

    Save token in cookie

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginService } from './login.service';
 import { AdminLogin } from './login.interface';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,7 @@ import { AdminLogin } from './login.interface';
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  constructor(private loginService: LoginService) {}
+  constructor(private loginService: LoginService, private cookieService: CookieService) {}
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -26,7 +27,7 @@ export class LoginComponent {
 
     this.loginService.login(admin).subscribe({
       next: (token) => {
-        console.log(token);
+        this.cookieService.set('token', token.accessToken, { expires: this.loginForm.get('keep')!.value! ? 30 : 1, sameSite: 'Strict' });
       },
       error: (err) => {
         console.log(err);
