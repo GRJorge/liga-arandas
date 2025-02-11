7f0d830b17ca54b0fbcff45f73caee0d9e59b005
GRJorge <deb.devel@outlook.es>
2025-02-11 01:30:39 -0600

    Home component not used

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { tokenGuard } from './guards/token.guard';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, title: 'Panel de administracion - Home', canActivate: [tokenGuard] },
   { path: 'login', component: LoginComponent, title: 'Login' },
 ];
