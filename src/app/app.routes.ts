f0ac5231ec1071ade8c9d98f6d1b8ea6d7529c2e
GRJorge <deb.devel@outlook.es>
2025-02-11 01:32:00 -0600

    Leagues component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { tokenGuard } from './guards/token.guard';
+import { LeaguesComponent } from './leagues/leagues.component';
 
 export const routes: Routes = [
+  { path: 'leagues', component: LeaguesComponent, title: 'Ligas', canActivate: [tokenGuard] },
   { path: 'login', component: LoginComponent, title: 'Login' },
 ];
---
+++ src/app/leagues/leagues.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leagues',
+  imports: [],
+  templateUrl: './leagues.component.html',
+})
+export class LeaguesComponent {}
