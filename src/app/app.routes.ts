c337a429033be7ea57ef838592ec035bcd8ef8df
GRJorge <deb.devel@outlook.es>
2025-02-12 01:49:49 -0600

    Create league form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { tokenGuard } from './guards/token.guard';
 import { LeaguesComponent } from './leagues/leagues.component';
+import { CreateLeagueComponent } from './leagues/create-league/create-league.component';
 
 export const routes: Routes = [
   { path: 'leagues', component: LeaguesComponent, title: 'Ligas', canActivate: [tokenGuard] },
+  { path: 'leagues/create', component: CreateLeagueComponent, canActivate: [tokenGuard] },
   { path: 'login', component: LoginComponent, title: 'Login' },
 ];
---
+++ src/app/leagues/create-league/create-league.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-league',
+  imports: [],
+  templateUrl: './create-league.component.html',
+})
+export class CreateLeagueComponent {}
