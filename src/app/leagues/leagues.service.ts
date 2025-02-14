acfc99bffaa2f39a3327c14e255fe56cfe263945
GRJorge <deb.devel@outlook.es>
2025-02-14 02:52:28 -0600

    Get all leagues

--- src/app/leagues/leagues.service.ts
+++ src/app/leagues/leagues.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { League } from './leagues.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +10,12 @@ import { environment } from '../../environments/environment';
 export class LeaguesService {
   constructor(private readonly http: HttpClient) {}
 
-  base = environment.apiUrl;
+  private base = environment.apiUrl + 'league';
 
   createLeague(league: FormData) {
-    return this.http.post(`${this.base}league/create`, league);
+    return this.http.post(`${this.base}/create`, league);
+  }
+  getAllLeagues() {
+    return this.http.get<League[]>(`${this.base}/get`);
   }
 }
