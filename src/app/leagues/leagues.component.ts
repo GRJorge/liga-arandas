e4b0e02b584bfeb1983d64ba318c263d1b79af56
GRJorge <deb.devel@outlook.es>
2025-02-21 00:43:55 -0600

    Redirection to panel

--- src/app/leagues/leagues.component.ts
+++ src/app/leagues/leagues.component.ts
@@ -2,19 +2,26 @@ import { Component } from '@angular/core';
 import { LeaguesService } from './leagues.service';
 import { League } from './leagues.interface';
 import { environment } from '../../environments/environment';
+import { CookieService } from 'ngx-cookie-service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-leagues',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './leagues.component.html',
 })
 export class LeaguesComponent {
   leagues?: League[];
-  imgUrl = environment.apiUrl + 'uploads/'
+  imgUrl = environment.apiUrl + 'uploads/';
 
-  constructor(private readonly leaguesService: LeaguesService) {
+  constructor(private readonly leaguesService: LeaguesService, private readonly cookieService: CookieService, private readonly router: Router) {
     leaguesService.getAllLeagues().subscribe((leagues) => {
       this.leagues = leagues;
     });
+  }
+
+  navigateToPanel(idLeague: string) {
+    this.cookieService.set('league', idLeague);
+    this.router.navigate(['/panel']);
   }
 }
