1f4fbdd94a2cd82aedd5878e847940517a782f71
GRJorge <deb.devel@outlook.es>
2025-02-25 23:56:44 -0600

    Categories component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { LeaguesComponent } from './leagues/leagues.component';
 import { CreateLeagueComponent } from './leagues/create-league/create-league.component';
 import { PanelComponent } from './panel/panel.component';
 import { leagueGuard } from './guards/league.guard';
+import { CategoriesComponent } from './categories/categories.component';
 
 export const routes: Routes = [
   { path: 'leagues', component: LeaguesComponent, title: 'Ligas', canActivate: [tokenGuard] },
   { path: 'leagues/create', component: CreateLeagueComponent, canActivate: [tokenGuard] },
   { path: 'login', component: LoginComponent, title: 'Login' },
   { path: 'panel', component: PanelComponent, title: 'Principal Panel', canActivate: [tokenGuard, leagueGuard] },
+  { path: 'categories', component: CategoriesComponent, title: 'Categories', canActivate: [tokenGuard, leagueGuard] },
 ];
