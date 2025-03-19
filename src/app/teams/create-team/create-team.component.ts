2a52e5546affcafb13117c9a9bc647936ddf4ddf
GRJorge <deb.devel@outlook.es>
2025-03-19 00:36:19 -0600

    Form validator

--- src/app/teams/create-team/create-team.component.ts
+++ src/app/teams/create-team/create-team.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-team',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './create-team.component.html',
 })
-export class CreateTeamComponent {}
+export class CreateTeamComponent {
+  createForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    shortName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]),
+    shield: new FormControl('', Validators.required),
+    category: new FormControl('', Validators.required),
+  });
+}
